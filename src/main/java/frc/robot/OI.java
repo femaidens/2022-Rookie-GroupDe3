e5254e0c8990564f926438e1fd27a2d8a47747b1
Jillian Chong <chongj@bxscience.edu>
2022-02-07 17:06:32 -0500

    updated shooter2

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 import edu.wpi.first.wpilibj.buttons.*;
-import frc.robot.Commands.Shoot;
 import frc.robot.Commands.shoot2;
 import frc.robot.Commands.reset;
 import edu.wpi.first.wpilibj.Joystick;
@@ -13,10 +12,8 @@ import edu.wpi.first.wpilibj.Joystick;
 public class OI {
     public static Joystick leftJoy = new Joystick(RobotMap.leftJoyPort);
     public static Joystick rightJoy = new Joystick(RobotMap.rightJoyPort);
-    public static Button shoot = new JoystickButton(leftJoy, RobotMap.shootPort);
     public static Button shoot2 = new JoystickButton(rightJoy, RobotMap.shoot2Port);
     public void bindButton(){
-        shoot.whenPressed(new Shoot());
         shoot2.toggleWhenPressed(new shoot2());
         shoot2.toggleWhenPressed(new reset());
         
