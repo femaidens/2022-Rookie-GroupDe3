ba74530443159ec747bd8e83a8fd1547385edc18
Jillian Chong <chongj@bxscience.edu>
2022-02-11 17:47:39 -0500

    limelight

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,24 +6,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.buttons.*;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.Commands.Shoot;
-import frc.robot.Commands.shoot2;
-import frc.robot.Commands.reset;
-import frc.robot.Commands.Intake;
 import frc.robot.*;
 
 /** Add your docs here. */
 public class OI {
     public static Joystick leftJoy = new Joystick(RobotMap.leftJoyPort);
     public static Joystick rightJoy = new Joystick(RobotMap.rightJoyPort);
-    public static Button shoot = new JoystickButton(leftJoy, RobotMap.shootPort);
-    public static Button shoot2 = new JoystickButton(rightJoy, RobotMap.shoot2Port);
-    public static Button intakeButton = new JoystickButton(rightJoy, RobotMap.intakeButtonPort);
     public void bindButton(){
-        shoot.whenPressed(new Shoot());
-        shoot2.toggleWhenPressed(new shoot2());
-        shoot2.toggleWhenPressed(new reset());
-        intakeButton.whileHeld(new Intake());
         
     }
 }
