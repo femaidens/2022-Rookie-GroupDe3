db35ca2228874514b648196f080225e3b47b0132
Jillian Chong <chongj@bxscience.edu>
2022-02-12 11:35:24 -0500

    clearing main

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,44 +6,4 @@ package frc.robot;
 
 /** Add your docs here. */
 public class RobotMap {
-    public static int leftPisPort = 0;
-    public static int rightPisPort = 1;
-    public static int joyPort = 2; 
-    public static int frontLeftPort = 3;
-    public static int frontRightPort = 4;
-    public static int rearLeftPort = 5;
-    public static int rearRightPort = 6;
-    public static int gyroPort = 7;
-    public static int leftJoyPort = 8;
-    public static int rightJoyPort = 9;
-    public static int shootPort = 10;
-    public static int rightJoyYPort = 11;
-    public static int rightJoyXPort = 12;
-    public static int leftJoyYPort = 13;
-    public static int leftJoyXPort = 14;
-    public static int intakeMotorPort = 15;
-    public static int leftIntakePisForwardPort = 16;
-    public static int leftIntakePisBackwardPort = 17;
-    public static int rightIntakePisForwardPort = 18;
-    public static int rightIntakePisBackwardPort = 19;
-    public static int ultrasonic1Port = 1;
-    public static int ultrasonic2Port = 2;
-    public static int Shooter2MotorPort = 20;
-    public static int shooterPisForwardPort = 21;
-    public static int shooterPisBackwardPort = 22;
-    public static int shooter2EncoderPort = 23;
-    public static int shoot2Port = 24;
-    public static int leftArmMotorPort = 25;
-    public static int rightArmMotorPort = 26;
-    public static int leftArmPisForwardPort = 27;
-    public static int leftArmPisBackwardPort = 28;
-    public static int rightArmPisForwardPort = 29;
-    public static int rightArmPisBackwardPort = 30;
-    public static int leftUltra1Port = 31;
-    public static int leftUltra2Port = 32;
-    public static int rightUltra1Port = 33;
-    public static int rightUltra2Port = 34;
-    public static int intakeButtonPort = 35;
-
-    
 }
