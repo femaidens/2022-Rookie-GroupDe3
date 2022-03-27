144532d3913555e8bd4d2145e628d4efd850dc73
Bronx High School of Science <School@Robotics-02.bxsci.78x445.nycboe.org>
2022-03-27 13:02:50 -0400

    canspark ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,28 +8,37 @@ import edu.wpi.first.wpilibj.DigitalOutput;
 
 /** Add your docs here. */
 public class RobotMap {
-    public static int joyPort = 2; 
-    public static int frontLeftPort = 3;
-    public static int frontRightPort = 4;
-    public static int rearLeftPort = 5;
+    public static int joyPort = 2; //change
+
+    public static int frontLeftPort = 1;
+    public static int frontRightPort = 2;
+    public static int rearLeftPort = 9;
     public static int rearRightPort = 6;
-    public static int gyroPort = 7;
-    public static int leftJoyPort = 8;
-    public static int rightJoyPort = 9;
+
+    public static int gyroPort = 7; //change
+
+    public static int leftJoyPort = 8; //change
+    public static int rightJoyPort = 9; //change
+
     public static int rightJoyYPort = 11;
     public static int rightJoyXPort = 12;
     public static int leftJoyYPort = 13;
+
     public static int leftJoyXPort = 14;
-    public static int leftArmMotorPort = 25;
-    public static int rightArmMotorPort = 26;
+
+    public static int leftArmMotorPort = 8;
+    public static int rightArmMotorPort = 7;
+
     public static int leftArmPisForwardPort = 27;
     public static int leftArmPisBackwardPort = 28;
     public static int rightArmPisForwardPort = 29;
     public static int rightArmPisBackwardPort = 30;
+
     public static int leftUltra1Port = 31;
     public static int leftUltra2Port = 32;
     public static int rightUltra1Port = 33;
     public static int rightUltra2Port = 34;
+
     public static int climbMidButtonPort = 35;
     public static int changeBarButtonPort = 36;
     public static int climbPIDButtonPort = 37;
