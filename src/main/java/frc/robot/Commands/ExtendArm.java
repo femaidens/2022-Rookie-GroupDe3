d7c8c31c2912a068107c676983b26330ca3562f8
Bronx High School of Science <School@Robotics-02.bxsci.78x445.nycboe.org>
2022-03-27 10:15:27 -0400

    added end

--- src/main/java/frc/robot/Commands/ExtendArm.java
+++ src/main/java/frc/robot/Commands/ExtendArm.java
@@ -63,5 +63,7 @@ public class ExtendArm extends Command {
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
-  protected void interrupted() {}
+  protected void interrupted() {
+    end();
+  }
 }
--- src/main/java/frc/robot/Commands/SpinWheel.java
+++ src/main/java/frc/robot/Commands/SpinWheel.java
@@ -40,5 +40,6 @@ public class SpinWheel extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
