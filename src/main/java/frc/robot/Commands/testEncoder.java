a36e75075f7e4ac7ff9f806618a5ca84d8cecd06
Bronx High School of Science <School@Robotics-02.bxsci.78x445.nycboe.org>
2022-03-27 10:18:18 -0400

    added end

--- src/main/java/frc/robot/Commands/testEncoder.java
+++ src/main/java/frc/robot/Commands/testEncoder.java
@@ -38,5 +38,7 @@ public class testEncoder extends Command {
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
-  protected void interrupted() {}
+  protected void interrupted() {
+    end();
+  }
 }
